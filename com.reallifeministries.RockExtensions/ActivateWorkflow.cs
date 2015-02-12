cfb7460a0e3a91c0a8150fe73ac71fa8aa4e85b8
Nik Petersen <nik@petersendata.net>
2015-02-11 16:28:45 -0800

    Started RockExtensions project.  Stubbed Activate Workflow  WorkflowAction,

---
+++ com.reallifeministries.RockExtensions/ActivateWorkflow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Workflow;
+
+namespace com.reallifeministries.RockExtensions
+{
+    /// <summary>
+    /// Activates a new activity for a given activity type
+    /// </summary>
+    [Description( "Activates a new workflow instance." )]
+    [Export( typeof( ActionComponent ) )]
+    [ExportMetadata( "ComponentName", "Activate Workflow" )]
+
+    [WorkflowTypeField( "Workflow", "The workflow type to activate", false,true,"","",0)]
+    public class ActivateWorkflow : ActionComponent
+    {
+        /// <summary>
+        /// Executes the specified workflow.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
+        public override bool Execute( RockContext rockContext, WorkflowAction action, Object entity, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+            return true;
+        }
+    }
+}
