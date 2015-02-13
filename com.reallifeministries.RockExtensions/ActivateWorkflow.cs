28fba23dcf8321e958e9a088fc5be71e2fb205f1
Nik Petersen <nik@petersendata.net>
2015-02-13 11:21:20 -0800

    Initial untested code for  workflow activation action
    ref #39

--- com.reallifeministries.RockExtensions/ActivateWorkflow.cs
+++ com.reallifeministries.RockExtensions/ActivateWorkflow.cs
@@ -23,6 +23,7 @@ namespace com.reallifeministries.RockExtensions
     [ExportMetadata( "ComponentName", "Activate Workflow" )]
 
     [WorkflowTypeField( "Workflow", "The workflow type to activate", false,true,"","",0)]
+    [TextField("WorkflowName", "What to name this Workflow Instance",true)]
     public class ActivateWorkflow : ActionComponent
     {
         /// <summary>
@@ -36,7 +37,66 @@ namespace com.reallifeministries.RockExtensions
         public override bool Execute( RockContext rockContext, WorkflowAction action, Object entity, out List<string> errorMessages )
         {
             errorMessages = new List<string>();
-            return true;
+            Guid guid = GetAttributeValue( action, "Workflow" ).AsGuid();
+            if (guid.IsEmpty())
+            {
+                action.AddLogEntry( "Invalid Workflow Property", true );
+                return false;
+            }
+
+            var currentActivity = action.Activity;
+            var currentWorkflow = action.Activity.Workflow;
+            var newWorkflowType = new WorkflowTypeService( rockContext ).Get( guid );
+            var newWorkflowName = GetAttributeValue( "WorkflowName" );
+
+            if (newWorkflowType == null)
+            {
+                action.AddLogEntry( "Invalid Workflow Property", true );
+                return false;
+            }
+
+            var newWorkflow = Workflow.Activate( newWorkflowType, GetAttributeValue("WorkflowName") );
+            if (newWorkflow == null)
+            {
+                action.AddLogEntry( "The Workflow could not be activated", true );
+                return false;
+            }
+
+            if (currentActivity.Attributes == null)
+            {
+                currentActivity.LoadAttributes( rockContext );
+            }
+
+            foreach (string key in currentActivity.AttributeValues.Keys)
+            {
+                newWorkflow.SetAttributeValue( key, currentActivity.GetAttributeValue(key) );
+            }
+
+
+            if(newWorkflow.Process( rockContext, entity, out errorMessages )) 
+            {
+                if (newWorkflow.IsPersisted || newWorkflowType.IsPersisted)
+                {
+                    var workflowService = new Rock.Model.WorkflowService( rockContext );
+                    workflowService.Add( newWorkflow );
+
+                    rockContext.WrapTransaction( () =>
+                    {
+                        rockContext.SaveChanges();
+                        newWorkflow.SaveAttributeValues( rockContext );
+                        foreach (var activity in newWorkflow.Activities)
+                        {
+                            activity.SaveAttributeValues( rockContext );
+                        }
+                    } );
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
