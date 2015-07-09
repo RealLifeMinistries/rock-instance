c37c284a44321b66b333cb26a98ff6778b9df381
Nik Petersen <nik@petersendata.net>
2015-07-09 13:54:39 -0700

    Updated Group matcher to catch liquid exceptions

---
+++ com.realifeministries/SystemGuid/DefinedValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.realifeministries.SystemGuid
+{
+    public static class DefinedValue
+    {
+        public const string REGION_LOCATION_TYPE = "0B0AE375-BC14-4839-8A04-69E152AC676C";
+    }
+}
